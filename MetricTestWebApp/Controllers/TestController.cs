36817ff80f34363370b6b2c1a176fae7d1e899c1
koudenpa <koudenpa@hotmail.com>
2018-12-09 14:31:30 +0900

    軽く負荷をかけるメソッドを追加した。

---
+++ MetricTestWebApp/Controllers/TestController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MetricTestWebApp.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private bool IsPrime(int i)
+        {
+            for (var j = 2; j < i; j++)
+            {
+                if (i % j == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        [HttpPost]
+        public IActionResult Cpu()
+        {
+            var primes = new List<int>();
+            foreach (var i in Enumerable.Range(2, 1000 * 100))
+            {
+                if (IsPrime(i))
+                {
+                    primes.Add(i);
+                }
+            }
+            Console.WriteLine(string.Join(", ", primes));
+            return Redirect("/");
+        }
+
+        [HttpPost]
+        public IActionResult Memory()
+        {
+            var strings = new List<string>();
+            foreach (var i in Enumerable.Range(1, 1000))
+            {
+                strings.Add(new string('*', 1000));
+            }
+            Console.WriteLine(string.Join(", ", strings.Select(x => x.Length)));
+            return Redirect("/");
+        }
+    }
+}
